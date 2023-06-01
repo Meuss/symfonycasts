2fa42d130f3faa1da1e360320eaf9111ea4ab383
Thomas Miller <thomas.miller147@gmail.com>
2023-06-01 16:12:39 +0200

    webpack encore settings

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,19 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+document.querySelectorAll('.js-vote-arrows a').forEach((link) => {
+    link.addEventListener('click', async (e) => {
+        e.preventDefault();
+        let direction = link.dataset.direction;
+        try {
+            let response = await fetch('/comments/10/vote/' + direction, {
+                method: 'POST'
+            });
+            let data = await response.json();
+            document.querySelectorAll('.js-vote-total').forEach(el => el.innerText = data.votes);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
+});
