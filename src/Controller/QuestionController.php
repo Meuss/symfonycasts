0d5ead3cc8a896341374ec4257ddd608bcfbd6e5
Thomas Miller <thomas.miller147@gmail.com>
2023-06-09 12:58:30 +0200

    :construction:

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 use App\Service\MarkdownHelper;
 use Psr\Log\LoggerInterface;
@@ -39,7 +40,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, CacheInterface $cache)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
@@ -53,7 +54,9 @@ class QuestionController extends AbstractController
 
         $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
-        $parsedQuestionText = $markdownHelper->parse($questionText);
+        $parsedQuestionText = $cache->get('markdown_' . md5($questionText), function () use ($questionText, $markdownHelper) {
+            return $markdownHelper->parse($questionText);
+        });
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
